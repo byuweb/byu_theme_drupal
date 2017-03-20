9df644404e1a9f2d2c824dffd91d1a7483123a90
katrialesser <katrialynn18@gmail.com>
2017-03-20 14:31:34 -0600

    adds a sub theme for easy sub theming - still testing this

---
+++ subtheme/js/scripts.js
@@ -0,0 +1,10 @@
+(function ($, Drupal) {
+
+  Drupal.behaviors.byu2017_d7 = {
+    attach: function(context, settings) {
+      // Make your changes here
+
+    }
+  };
+
+})(jQuery, Drupal);
---
+++ subtheme/theme-settings.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Implements hook_form_FORM_ID_alter().
+ *
+ * @param $form
+ *   The form.
+ * @param $form_state
+ *   The form state.
+ */
+function subtheme_form_system_theme_settings_alter(&$form, &$form_state) {
+
+}
