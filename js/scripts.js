17877d14817169af5103e9f388f82e7637dbeb0e
Katria Hale <katrialynn18@gmail.com>
2017-05-24 14:01:46 -0600

    adding chrome class

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,18 @@
 (function ($, Drupal) {
 
+
+    var userAgent = navigator.userAgent.toLowerCase();
+    if (userAgent.indexOf("chrome") != -1){
+      $('html').addClass('chrome');
+    }
+
+
+
   Drupal.behaviors.byu2017_d7 = {
     attach: function(context, settings) {
       // Make your changes here
+
+
 
     }
   };
