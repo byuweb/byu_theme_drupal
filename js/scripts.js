e855784885111af6fd96d60226e784e60dd8cdc0
Katria Hale <katrialynn18@gmail.com>
2017-05-24 14:25:27 -0600

    unsetting things for buttons an dicons in search from foundation

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,6 @@
 (function ($, Drupal) {
 
 
-    var userAgent = navigator.userAgent.toLowerCase();
-    if (userAgent.indexOf("chrome") != -1){
-      $('html').addClass('chrome');
-    }
-
-
-
   Drupal.behaviors.byu2017_d7 = {
     attach: function(context, settings) {
       // Make your changes here
