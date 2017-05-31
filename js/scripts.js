796476616ce73f25584bfd0a060bba7e8a18c733
Katria Lesser <katrialesser@users.noreply.github.com>
2017-05-31 10:23:24 -0600

    fixing submit on search (#15)

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
 (function ($, Drupal) {
-
+  // from byu-theme-components documentation for default drupal 7 search form:
+  function d7SearchComplex(value) {
+    document.getElementById('edit-submit--2').click();
+  }
 
   Drupal.behaviors.byu2017_d7 = {
     attach: function(context, settings) {
-      // Make your changes here
-
-
 
     }
   };
