f8c268fc1997dfd2c43b4cb4bfd0c62e9cc327a5
Blake Morgan <b.morgan.296@gmail.com>
2018-10-10 11:58:42 -0600

    Updated gulp and fix all npm vulnerabilities

--- gulpfile.js
+++ gulpfile.js
@@ -9,12 +9,13 @@ var inject = require('gulp-inject');
 var minify = require('gulp-minify');
 
 
-gulp.task('styles', function() {
+gulp.task('styles', function(done) {
     gulp.src('./scss/*.scss')
         .pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
         .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('./css/'));
+    done();
 });
 
 // maybe comment later?
@@ -25,10 +26,10 @@ gulp.task('minify-css', function() {
 });
 
 //Watch task
-gulp.task('default',['styles'], function() {
+gulp.task('default', gulp.series('styles', function() {
   gulp.watch('scss/*.scss');
-});
+}));
 
 //Distribution task
-gulp.task('css', ['styles', 'minify-css']);
+gulp.task('css', gulp.series('styles', 'minify-css'));
 
